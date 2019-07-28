e8bf61c67e20b63d797dfb6380d7962505e1fa8f
robin_time <robin_time@163.com>
2019-07-28 15:55:50 +0800

    架构公共部分优化

--- src/main/java/com/lxy/stuinfomp/service/reg/StuinfompServiceRegApplication.java
+++ src/main/java/com/lxy/stuinfomp/service/reg/StuinfompServiceRegApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lxy.stuinfomp" )
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.lxy.stuinfomp.commons.mapper")
 public class StuinfompServiceRegApplication {
--- src/main/java/com/lxy/stuinfomp/service/reg/controller/RegController.java
+++ src/main/java/com/lxy/stuinfomp/service/reg/controller/RegController.java
@@ -1,23 +1,44 @@
 package com.lxy.stuinfomp.service.reg.controller;
 
 import com.lxy.stuinfomp.commons.domain.Users;
-import com.lxy.stuinfomp.commons.mapper.UsersMapper;
+import com.lxy.stuinfomp.commons.dto.AbstractBaseResult;
+import com.lxy.stuinfomp.commons.service.UserService;
+import com.lxy.stuinfomp.commons.validator.BeanValidator;
+import com.lxy.stuinfomp.commons.web.AbstractBaseController;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "reg")
-public class RegController {
+public class RegController extends AbstractBaseController<Users> {
 
     @Autowired
-    private UsersMapper usersMapper;
+    private UserService userService;
 
-    @GetMapping(value = {"{id}"})
-    public String reg(@PathVariable int id){
-        Users user = usersMapper.selectByPrimaryKey(id);
-        return user.getUserName();
+    @PostMapping(value = "")
+    public AbstractBaseResult reg(Users user){
+        String message = BeanValidator.validator(user);
+        if (StringUtils.isNotBlank(message)){
+            return error(message,null);
+
+        }
+
+        /**
+         * 验证用户名是否重复
+         */
+        if (!userService.unique("userName",user.getUserName())){
+            return error("用户名重复，请重试",null);
+        }
+
+        /**
+         * 验证邮箱是否重复
+         */
+        if (!userService.unique("email",user.getEmail())){
+            return error("邮箱重复，请重试",null);
+        }
+        return null;
     }
 }
