c9a58a2958b7354bc7f7d60ebdc38f791c8573b3
robin_time <robin_time@163.com>
2019-07-28 19:13:10 +0800

    mapper工具类，用户注册成功后发邮件暂时不做

--- src/main/java/com/lxy/stuinfomp/service/reg/StuinfompServiceRegApplication.java
+++ src/main/java/com/lxy/stuinfomp/service/reg/StuinfompServiceRegApplication.java
@@ -3,11 +3,13 @@ package com.lxy.stuinfomp.service.reg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(scanBasePackages = "com.lxy.stuinfomp" )
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.lxy.stuinfomp.commons.mapper")
+@EnableAsync
 public class StuinfompServiceRegApplication {
     public static void main(String[] args) {
         SpringApplication.run(StuinfompServiceRegApplication.class,args);
