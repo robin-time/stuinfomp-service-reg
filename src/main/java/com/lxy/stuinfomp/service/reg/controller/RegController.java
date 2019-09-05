e8f4d6dff99e0a9a9cfa11f10a3f2f4750659c1c
robin_time <robin_time@163.com>
2019-09-06 00:31:48 +0800

    user list接口

--- src/main/java/com/lxy/stuinfomp/service/reg/controller/RegController.java
+++ src/main/java/com/lxy/stuinfomp/service/reg/controller/RegController.java
@@ -1,23 +1,25 @@
 package com.lxy.stuinfomp.service.reg.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.lxy.stuinfomp.commons.domain.Users;
 import com.lxy.stuinfomp.commons.dto.AbstractBaseResult;
 import com.lxy.stuinfomp.commons.service.UserService;
 import com.lxy.stuinfomp.commons.validator.BeanValidator;
 import com.lxy.stuinfomp.commons.web.AbstractBaseController;
 import com.lxy.stuinfomp.service.reg.entity.UserDTO;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "user")
 public class RegController extends AbstractBaseController<Users> {
@@ -76,4 +78,23 @@ public class RegController extends AbstractBaseController<Users> {
         }
         return error("用户名或密码错误",null);
     }
+
+    @ApiOperation(value = "用户信息分页查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页码", required = true, paramType = "path"),
+            @ApiImplicitParam(name = "pageSize", value = "笔数", required = true, paramType = "path")
+    })
+    @GetMapping(value = "page/{pageNum}/{pageSize}")
+    public AbstractBaseResult selectCourses(@ApiParam(name = "用户信息", required = false) Users user,
+                                            @PathVariable int pageNum,
+                                            @PathVariable int pageSize){
+        try {
+            PageInfo<Users> pageInfo = userService.page(user, pageNum, pageSize);
+            return success(request.getRequestURI(),pageInfo.getNextPage(),pageInfo.getPages(),pageInfo.getList());
+        } catch (Exception e) {
+            log.error("CourseController.selectCourses() error = {}",e);
+        }
+        return error("查询失败，请重试",null);
+    }
+
 }
